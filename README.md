ae917eb65b5656d62a0f4ddeb178b8e1610f68e2
Diego Carrasco <557703+dacog@users.noreply.github.com>
2020-05-09 11:13:15 +0200

    fix git history

---
+++ README.md
@@ -0,0 +1,14 @@
+# Springer Textbook downloader
+
+This small script downloads the textbooks available in [SpringerNature](https://www.springernature.com/gp/librarians/news-events/all-news-articles/industry-news-initiatives/free-access-to-textbooks-for-institutions-affected-by-coronaviru/17855960) because of Coronavirus outbreak.
+
+This script will create a `downloads/english` and a `downloads/german` forlder and download all the available pdfs.
+
+To run in a virtualenv 
+
+```bash
+virtualenv -p python3 venv
+source venv/bin/activate
+pip -r requirements.txt
+python download_springer.py
+```
---
+++ download_springer.py
@@ -0,0 +1,36 @@
+import requests
+import os
+import pandas as pd
+import logging
+
+logger = logging.getLogger('download_springer_textbooks')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+
+urls = {
+    'english': 'https://resource-cms.springernature.com/springer-cms/rest/v1/content/17858272/data/v5',
+    'german': 'https://resource-cms.springernature.com/springer-cms/rest/v1/content/17863240/data/v2'
+}
+
+DOWNLOAD_FOLDER = 'downloads/'
+logger.info('Processing downloads')
+for language, url in urls.items():
+    logger.info('Processing download for %s', language)
+    df = pd.read_excel(url)
+    download_path = DOWNLOAD_FOLDER + language
+    if not os.path.exists(download_path):
+        logger.info('Creating folder %s', download_path)
+        os.makedirs(download_path)
+    for index, row in df.iterrows():
+        file_name = f"{row.loc['Book Title']}_{row.loc['Edition']}"
+        download_url = f"{row.loc['OpenURL']}"
+        logger.info('Downloading %s', file_name)
+        with open('{}/{}.pdf'.format(download_path, file_name), 'wb') as f:
+            f.write(requests.get(url).content)
+
+
